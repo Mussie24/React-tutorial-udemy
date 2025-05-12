17874a88f970a43b8cc48339f77af434cd3d8333
Mussie Mahdere <mussie@aleksundshantu.com>
2025-05-12 17:21:09 +0200

    useState-page starts with contents from component

---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+export default function CoreConcept({title, description, image}) {
+  return (
+    <li>
+      <img src={image} alt="..." />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}

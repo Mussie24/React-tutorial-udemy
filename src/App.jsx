625d6fb8985c6995d027116774b4d794894cd997
Mussie Mahdere <mussie@aleksundshantu.com>
2025-05-12 18:00:54 +0200

    lesson 56 done

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import { EXAMPLES } from './data.js';
 
 
 function App() {
-  const [selectedTopic, setSelectedTopic]= useState('components');
+  const [selectedTopic, setSelectedTopic]= useState();
 
   
 
@@ -52,14 +52,15 @@ function App() {
             <TabButton onSelect={()=>handleSelect('props')}>Props</TabButton>    
             <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>  
           </menu>
-          <div id='tab-content'>
-            
+          {!selectedTopic && <p>Please select a topic</p>}
+          {selectedTopic &&  (<div id='tab-content'>
             <h3>{EXAMPLES[selectedTopic].title}</h3>
             <p>{EXAMPLES[selectedTopic].description}</p>
             <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
+              <code>{EXAMPLES[selectedTopic].code}</code> 
             </pre>
-          </div>
+          </div> )}
+          
         </section>
         
       </main>
